1339964cbb051b90ba65b6b06c4dcad53bf3a059
Lliam <lliam@iMac8.local>
2019-05-14 11:17:14 -0400

    made alert

--- WeightTracker3000/WeightTracker3000/ViewController.swift
+++ WeightTracker3000/WeightTracker3000/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentWeight: UITextField!
+    @IBOutlet weak var goalWeight: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func checkAndMoveOn(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Error In Entered Values", message: "Please make sure you are entering correct and healthy weight values", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Re-Enter", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+        // https://learnappmaking.com/uialertcontroller-alerts-swift-how-to/
     }
 
 
